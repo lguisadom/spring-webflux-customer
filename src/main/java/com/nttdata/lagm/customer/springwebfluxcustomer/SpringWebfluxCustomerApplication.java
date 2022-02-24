46cab4397e4c44b66c2f8f1cb51b556e4c1f506e
Luis Guisado <lguisadom@gmail.com>
2022-02-24 16:23:06 -0500

    Begin

---
+++ src/main/java/com/nttdata/lagm/customer/springwebfluxcustomer/SpringWebfluxCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.lagm.customer.springwebfluxcustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxCustomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/lagm/customer/springwebfluxcustomer/SpringWebfluxCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.lagm.customer.springwebfluxcustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
