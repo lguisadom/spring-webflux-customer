793d1dfec4418497ec2c4aaf2f8a30572fff8cba
Luis Guisado <lguisadom@gmail.com>
2022-02-28 12:36:30 -0500

    Anotaciones mongodb

--- src/main/java/com/nttdata/lagm/customer/model/Customer.java
+++ src/main/java/com/nttdata/lagm/customer/model/Customer.java
@@ -4,14 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Document
+@Document(collection = "customer")
 public class Customer {
+	@Id
     private Long id;
     private String lastName;
     private String firstName;
--- src/main/java/com/nttdata/lagm/customer/model/CustomerType.java
+++ src/main/java/com/nttdata/lagm/customer/model/CustomerType.java
@@ -4,14 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Document
+@Document(collection = "customerType")
 public class CustomerType {
+	@Id
     private Integer id;
     private String description;
 }
