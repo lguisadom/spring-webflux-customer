ed01401655c598e6912996dfdc3bc42a6b4ead53
Luis Guisado <lguisadom@gmail.com>
2022-03-14 10:47:26 -0500

    Fix Model Document Customer Type

--- src/main/java/com/nttdata/lagm/customer/model/CustomerType.java
+++ src/main/java/com/nttdata/lagm/customer/model/CustomerType.java
@@ -12,7 +12,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Document(collection = "customerProfile")
+@Document(collection = "customerType")
 public class CustomerType {
 	@Id
     private Integer id;
